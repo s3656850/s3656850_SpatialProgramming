a1a49f71250d828017cc6e8cb698de54060e3bc6
Caley Manzie <89324405+s3656850@users.noreply.github.com>
2021-09-29 15:38:51 +1000

    Update Expop_priority.py

--- Expop_priority.py
+++ Expop_priority.py
@@ -2,7 +2,9 @@ from qgis.core import *
 from qgis.gui import *
 
 @qgsfunction(args='auto', group='Custom')
+#defining function EP
 def EP(Pop_rate, Expend_rate, feature, parent):
+    #creating loop of classification randing from Priority 1 - 5 depending on the classification from function money and people
     if Pop_rate == 'high' and Expend_rate == 'high':
         b = 'Priority 1'
     elif Pop_rate == 'high' and Expend_rate == 'medium':
