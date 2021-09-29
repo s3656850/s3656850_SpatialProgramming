40b70c45d8bbdddbb3ba675a50d10ee225a57185
Caley Manzie <89324405+s3656850@users.noreply.github.com>
2021-09-29 15:37:23 +1000

    Update Population.py

--- Population.py
+++ Population.py
@@ -2,14 +2,18 @@ from qgis.core import *
 from qgis.gui import *
 
 @qgsfunction(args='auto', group='Custom')
+#defining function
 def people(Pop, feature, parent):
+    #if the Pop value is less than 35,000 classify it low
     if int(Pop) < int(35000):
         b = 'low'
+    #if the Pop value is between 35,000 and 80,000 classify it medium
     elif int(Pop) >= int(35000) and int(Pop) < int(80000):
         b = 'medium'
+    #if the Pop value is greater than 80,000 classify it high
     elif int(Pop) >= int(80000):
         b = 'high'
-    #when 0 say null 
+    #if Pop is anything else classify it null 
     else:
         b = 'null'
     return b
